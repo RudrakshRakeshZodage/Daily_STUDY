9ad74b0e68306e7165aedae4c58cd44c1c7363ed
Rudraksh Rakesh Zodage <your-email@gmail.com>
2026-02-16 10:54:26 +0530

    Day-21.2

--- Day-21/1.py
+++ Day-21/1.py
@@ -20,5 +20,19 @@ class Solution:
         backtrack(0, [], 0)
         return res
 
-
+# LOGIC :- 
 # Question: Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target. Each number in candidates may only be used once in the combination. The solution set must not contain duplicate combinations.
+
+# Sort → group duplicates
+
+# Backtrack → try all combinations
+
+# Skip duplicates
+
+# if i > start and candidates[i] == candidates[i-1]:
+#     continue
+
+
+# Use once → call backtrack(i + 1, ...)
+
+# 👉 Core idea: DFS + no reuse + skip same-level duplicates 🔥
